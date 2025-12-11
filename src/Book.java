2af5ee4800462f7a3738f4f9c529396b88102dd9
anelya07 <253652@astanait.edu.kz>
2025-12-11 17:21:25 +0500

    2

---
+++ src/Book.java
@@ -0,0 +1,46 @@
+public class Book {
+    public String title;
+    public String author;
+    public int year;
+    public String genre;
+
+    public Book(String title, String author, int year, String genre) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
+
